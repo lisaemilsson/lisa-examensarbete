1424fd77c97ae5dc92592aae573df7c0ee9fd9ce
Lisa Emilson <emilson.matilda@gmail.com>
2024-06-19 10:21:13 +0200

    Lagt till heading taggar

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,7 +7,7 @@ function ProjectCard({ title, description, image, slug }: Project) {
       <div className="border border-slate-100 rounded-lg overflow-hidden">
         <img src={image} className=" object-cover w-full h-96 mb-4" alt="" />
         <div className="p-4">
-          <p className="mb-4 text-2xl font- text-gray-800">{title}</p>
+          <h2 className="mb-4 text-2xl font- text-gray-800">{title}</h2>
           <p className="text-lg text-gray-500">{description}</p>
         </div>
       </div>
